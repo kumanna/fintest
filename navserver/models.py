b8a9ac4ddbeca83ffd34fcae094ab5666e0184ca
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2016-08-26 13:10:59 +0530

    Pretty print models

--- navserver/models.py
+++ navserver/models.py
@@ -3,7 +3,13 @@ from django.db import models
 class MutualFund(models.Model):
     amfisymbol = models.CharField(max_length=10, unique=True)
 
+    def __str__(self):
+        return self.amfisymbol
+
 class MutualFundNAV(models.Model):
     mf = models.ForeignKey(MutualFund, related_name="nav", on_delete=models.CASCADE)
     date = models.DateField(db_index=True)
     nav = models.DecimalField(max_digits=20,decimal_places=6)
+
+    def __str__(self):
+        return "%s, %s, %s" % (self.mf.amfisymbol, str(self.date), str(self.nav))
