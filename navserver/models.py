c359987c33386541f41b102081f8b9ca38ab3e5f
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2016-08-26 13:05:29 +0530

    Add models

--- navserver/models.py
+++ navserver/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class MutualFund(models.Model):
+    amfisymbol = models.CharField(max_length=10, unique=True)
+
+class MutualFundNAV(models.Model):
+    mf = models.ForeignKey(MutualFund, related_name="nav", on_delete=models.CASCADE)
+    date = models.DateField(db_index=True)
+    nav = models.DecimalField(max_digits=20,decimal_places=6)
