407c3543eb80bb061c046152dd63a90a3108bd3f
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2016-08-26 19:10:23 +0530

    Improve URL scheme for date range

--- navserver/urls.py
+++ navserver/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'navserver'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<amfisymbol>[0-9]{6})/(?P<startdate>[0-9]{8})/(?P<enddate>[0-9]{8})/$', views.navjson, name='navjson'),
-    url(r'^v/(?P<amfisymbol>[0-9]{6})/(?P<startdate>[0-9]{8})/(?P<enddate>[0-9]{8})/$', views.navview, name='navview'),
+    url(r'^json/(?P<amfisymbol>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.navjson, name='navjson'),
+    url(r'^v/(?P<amfisymbol>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.navview, name='navview'),
 ]
