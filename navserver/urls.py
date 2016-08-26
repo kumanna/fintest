754353ce4929a1281c812367a5e79cd189ad63cf
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2016-08-26 13:38:16 +0530

    Handle MF datewise query

--- navserver/urls.py
+++ navserver/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<amfisymbol>[0-9]{6})/(?P<startdate>[0-9]{8})/(?P<enddate>[0-9]{8})/$', views.navjson, name='navjson'),
 ]
--- navserver/views.py
+++ navserver/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+
+from .models import MutualFund, MutualFundNAV
 
 def index(request):
     return HttpResponse("Hello, world. You're at the NAV server index.")
+
+def navjson(request, amfisymbol, startdate, enddate):
+    try:
+        mf = MutualFund.objects.get(amfisymbol=amfisymbol)
+    except:
+        raise Http404("MF does not exist!")
+    return HttpResponse("You asked for NAV data for %s from %s to %s."
+                            % (amfisymbol, startdate, enddate))
