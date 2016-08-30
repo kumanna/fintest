32004c9e5ee1d00ec2b41a76d44af56626a08dd5
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2016-08-30 11:08:19 +0530

    Add lumpsum compare feature

--- navserver/urls.py
+++ navserver/urls.py
@@ -6,5 +6,7 @@ app_name = 'navserver'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^json/(?P<amfisymbol>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.navjson, name='navjson'),
+    url(r'^json/(?P<amfisymbol1>[0-9]{6}),(?P<amfisymbol2>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.lumpsumcompare, name='lumpsumcompare'),
+    url(r'^v/(?P<amfisymbol1>[0-9]{6}),(?P<amfisymbol2>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.lumpsumview, name='lumpsumview'),
     url(r'^v/(?P<amfisymbol>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.navview, name='navview'),
 ]
--- navserver/views.py
+++ navserver/views.py
@@ -23,11 +23,29 @@ def navlist(amfisymbol, startdate, enddate):
     hyphenate_date = lambda x : x[:4] + '-' + x[4:6] + '-' + x[6:]
     startdate = hyphenate_date(startdate)
     enddate = hyphenate_date(enddate)
-    nav_values = mf.nav.filter(date__range = (startdate, enddate))
+    nav_values = mf.nav.filter(date__range = (startdate, enddate)).order_by('date')
     return list(nav_values.values('date', 'nav'))
 
 def navjson(request, amfisymbol, startdate, enddate):
     return JsonResponse(navlist(amfisymbol, startdate, enddate), safe=False)
+
+def lumpsumcompare(request, amfisymbol1, amfisymbol2, startdate, enddate):
+    nav_list_1 = navlist(amfisymbol1, startdate, enddate)
+    nav_list_1_multiple = 10000.0 / float(nav_list_1[0]['nav'])
+    print(nav_list_1[0]['nav'])
+    print(nav_list_1_multiple)
+    nav_list_1 = [{"date" : i['date'],
+                       'value' : '{:.2f}'.format(nav_list_1_multiple * float(i['nav']))}
+                       for i in nav_list_1]
+    nav_list_2 = navlist(amfisymbol2, startdate, enddate)
+    nav_list_2_multiple = 10000.0 / float(nav_list_2[0]['nav'])
+    print(nav_list_2[0]['nav'])
+    print(nav_list_2_multiple)
+    print(nav_list_2_multiple * float(nav_list_2[0]['nav']))
+    nav_list_2 = [{"date" : i['date'],
+                       'value' : '{:.2f}'.format(nav_list_2_multiple * float(i['nav']))}
+                       for i in nav_list_2]
+    return JsonResponse([nav_list_1, nav_list_2], safe=False)
 
 def navview(request, amfisymbol, startdate, enddate):
     try:
@@ -41,3 +59,20 @@ def navview(request, amfisymbol, startdate, enddate):
         'startdate' : startdate,
         'enddate' : enddate,}
     return HttpResponse(template.render(context, request))
+
+def lumpsumview(request, amfisymbol1, amfisymbol2, startdate, enddate):
+    try:
+        mf1 = MutualFund.objects.get(amfisymbol=amfisymbol1)
+        mf2 = MutualFund.objects.get(amfisymbol=amfisymbol2)
+    except:
+        raise Http404("MF does not exist!")
+    template = loader.get_template('navserver/navcompare.html')
+    context = {
+        'amfisymbol1' : amfisymbol1,
+        'mfname1' : mf1.mfname,
+        'amfisymbol2' : amfisymbol2,
+        'mfname2' : mf2.mfname,
+        'startdate' : startdate,
+        'enddate' : enddate,}
+    return HttpResponse(template.render(context, request))
+    
