02f7ec8d26193c888c503b4e29a5b97e4af32a94
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2016-09-04 16:22:29 +0530

    Alter URLs to be more friendy

--- navserver/urls.py
+++ navserver/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'navserver'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^json/(?P<amfisymbol>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.navjson, name='navjson'),
-    url(r'^json/(?P<amfisymbol1>[0-9]{6}),(?P<amfisymbol2>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.lumpsumcompare, name='lumpsumcompare'),
-    url(r'^v/(?P<amfisymbol1>[0-9]{6}),(?P<amfisymbol2>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.lumpsumview, name='lumpsumview'),
-    url(r'^v/(?P<amfisymbol>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.navview, name='navview'),
+    url(r'^(?P<amfisymbol>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8}).json$', views.navjson, name='navjson'),
+    url(r'^(?P<amfisymbol1>[0-9]{6}),(?P<amfisymbol2>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8}).json$', views.lumpsumcompare, name='lumpsumcompare'),
+    url(r'^(?P<amfisymbol1>[0-9]{6}),(?P<amfisymbol2>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.lumpsumview, name='lumpsumview'),
+    url(r'^(?P<amfisymbol>[0-9]{6})/(?P<startdate>[0-9]{8})-(?P<enddate>[0-9]{8})/$', views.navview, name='navview'),
 ]
